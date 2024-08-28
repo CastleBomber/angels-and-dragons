4caf72cfee4d25576688a0488ff81e2151c5897a
CastleBomber <richie.ucsb@gmail.com>
2024-08-28 01:28:47 -0700

    updating zone audio script to mix with menu audio feature

---
+++ Scripts-VR/Light-and-Sound-Show-Scripts/ZoneAudio.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Plays audio with volume when player is detected inside sphere collider
+public class ZoneAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public SphereCollider sphereCollider; // audio zone
+    public Transform player;
+    private bool isMenuOverriding = false; // Flag to check if menu is overriding
+
+    void Start()
+    {
+        // Game starts off with music playing, not audible until inside zone
+        audioSource.volume = 0.0f;
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    void Update()
+    {
+        // Center of the sphere collider
+        Vector3 sphereCenter = sphereCollider.transform.position + sphereCollider.center;
+
+        // Calculate the distance between the object and the center of the sphere
+        float distance = Vector3.Distance(player.position, sphereCenter);
+
+        // Check if distance is less than radius of the sphere collider (localScale.x accounts for scaling)
+        if (distance < sphereCollider.radius * sphereCollider.transform.localScale.x)
+        {
+            Debug.Log("Player inside of sphere");
+            audioSource.volume = 1.0f;
+		} 
+        else
+        {
+            Debug.Log("Player outside of sphere");
+            audioSource.volume = 0.0f;
+        }
+    }
+}
--- Scripts-VR/Menu-Warp-Scripts/WarpToArea.cs
+++ Scripts-VR/Menu-Warp-Scripts/WarpToArea.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Warp to designated area
+// Warp to designated area (need a dynamic way to reference area)
 public class WarpToArea : MonoBehaviour
 {
-    private Vector3 warpLocation; // x, y, z coordinates
-
-    // What if ships move? Need a dynamic way to reference area
+    public Transform warpTarget; // Transform reference to warp target (new)
+    private Transform Player;
+    public GameObject menu;
 
     void Start()
     {
-        
+        GameObject player = GameObject.FindWithTag("Player");
+        Player = player.transform;
     }
 
-    void Update()
+	// Button clicked , take input on menu and warp to that location
+	public void Warp()
     {
-        // Take input on menu
-        // Teleport to that location
-        
+        // Warp Player to target
+        Player.position = warpTarget.position;
+
+        // Turns controller back on after button turned it off prevent interference with position
+        Player.GetComponent<CharacterController>().enabled = true;
+
+        CloseMenu();
+    }
+
+    public void CloseMenu()
+    {
+        menu.SetActive(false);
     }
 }
